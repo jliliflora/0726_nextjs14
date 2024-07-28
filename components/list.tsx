4226db552e01cab5fc02c0e7a7d5130d899c4137
jliliflora <syon704@gmail.com>
2024-07-29 01:33:51 +0900

    prefetch 추가, 로딩페이지, 에러페이지 추가

--- components/list.tsx
+++ components/list.tsx
@@ -17,7 +17,7 @@ export default function List({ id, list_name }:IListProps) {
 
     return (
         
-            <Link href={`/list/${id}`}>
+            <Link prefetch href={`/list/${id}`}>
                 <li onClick={onClick} className={styles.list}>
                     {list_name} ➡️
                 </li>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -7,10 +7,10 @@ export default function Navigation() {
         <nav className={styles.nav}>
             <ul>
                 <li>
-                    <Link href="/">Home</Link>
+                    <Link prefetch href="/">Home</Link>
                 </li>
                 <li>
-                    <Link href="/about">About</Link>
+                    <Link prefetch href="/about">About</Link>
                 </li>
             </ul>
         </nav>
